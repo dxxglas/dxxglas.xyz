12b1c5495fee14e54fe8bcd4b2e2ae33749a2b72
dxxglas <mcavalcante.douglas@gmail.com>
2020-07-22 13:56:52 -0300

    f-mvp: change name and favicon

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -2,9 +2,5 @@ import * as React from "react";
 import "./Footer.scss";
 
 export const Footer = (): JSX.Element => {
-  return (
-    <div>
-      <h1>Footer</h1>
-    </div>
-  );
+  return <div></div>;
 };
