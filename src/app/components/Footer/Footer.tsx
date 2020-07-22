dca04cfc178b478924afdea5d5745a98f6406f67
dxxglas <mcavalcante.douglas@gmail.com>
2020-07-22 13:35:56 -0300

    f-mvp: restructuring of files and folders

---
+++ src/app/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import "./Footer.scss";
+
+export const Footer = (): JSX.Element => {
+  return (
+    <div>
+      <h1>Footer</h1>
+    </div>
+  );
+};
---
+++ src/app/pages/Home.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+import { Header } from "../components/Header/Header";
+import { Footer } from "../components/Footer/Footer";
+
+export const Home = (): JSX.Element => {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+};
