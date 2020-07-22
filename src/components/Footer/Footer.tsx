db5e3f57fa17c5883160a875a0490cc2d66d68a3
dxxglas <mcavalcante.douglas@gmail.com>
2020-07-22 01:43:25 -0300

    f-mvp: add components structure

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+import "./Footer.scss";
+
+export class Footer extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Footer</h1>
+      </div>
+    );
+  }
+}
