eecb10bc3d45992573c6af2e7ea6dc584e12acd4
dxxglas <mcavalcante.douglas@gmail.com>
2020-07-29 17:07:01 -0300

    f-footer: style social icons

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -13,22 +13,28 @@ export const Footer = (): JSX.Element => {
       <div className="footerContact glitchOnHover" data-text="Contact">
         <a href="mailto:me@dxxglas.xyz">Contact</a>
       </div>
-      <div className="footerSocial">
-        <SocialIcon
-          name="linkedin"
-          href="https://linkedin.com/in/dxxglas"
-          icon={LinkedIn}
-        />
-        <SocialIcon
-          name="github"
-          href="https://github.com/dxxglas"
-          icon={GitHub}
-        />
-        <SocialIcon
-          name="behance"
-          href="https://behance.net/dxxglas"
-          icon={Behance}
-        />
+      <div className="footerBox">
+        <div className="footerLanguage">
+          <p>EN</p>
+          <p>PT</p>
+        </div>
+        <div className="footerSocial">
+          <SocialIcon
+            name="linkedin"
+            href="https://linkedin.com/in/dxxglas"
+            icon={LinkedIn}
+          />
+          <SocialIcon
+            name="github"
+            href="https://github.com/dxxglas"
+            icon={GitHub}
+          />
+          <SocialIcon
+            name="behance"
+            href="https://behance.net/dxxglas"
+            icon={Behance}
+          />
+        </div>
       </div>
     </footer>
   );
