15413de34496901ec80d0851707228efa3ea2c94
dxxglas <mcavalcante.douglas@gmail.com>
2021-03-12 14:24:40 -0300

    f-home: adding router strucuture

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,30 @@
 import * as React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.scss";
 import "../providers/i18n";
 
 import { Navbar } from "./components/Navbar/Navbar";
 import { Menu } from "./components/Menu/Menu";
 
+import { Home } from "./pages/Home/Home";
+
 export const App = () => {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
   return (
     <div className="app">
-      <Navbar setMenuStatus={setIsMenuOpen} isOpen={isMenuOpen} />
-      <Menu setMenuStatus={setIsMenuOpen} isOpen={isMenuOpen} />
-      <div className="background"></div>
+      <Router>
+        <div className="background"></div>
+        <Navbar setMenuStatus={setIsMenuOpen} isOpen={isMenuOpen} />
+        <Menu setMenuStatus={setIsMenuOpen} isOpen={isMenuOpen} />
+        <div className="content">
+          <Switch>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 };
