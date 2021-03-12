0476c6cb05e28bf1a16f30e8c29b5a40c5b4146c
dxxglas <mcavalcante.douglas@gmail.com>
2021-03-12 13:27:33 -0300

    f-nav: menu setting for multiple languages

--- src/app/components/Menu/Menu.tsx
+++ src/app/components/Menu/Menu.tsx
@@ -4,22 +4,25 @@ import "./Menu.scss";
 import { useTranslation } from "react-i18next";
 
 export const Menu = ({ isOpen, setMenuStatus }: any): JSX.Element => {
-  const menu = [
-    {
-      title: "about",
-      subtitle: "###",
-    },
-    {
-      title: "portfolio",
-      subtitle: "###",
-    },
-  ];
-
   const { t, i18n } = useTranslation();
+
+  let language: string = i18n.language;
+  let languageSecondary: string = i18n.language === "en" ? "br" : "en";
 
   const changeLanguage = (value: string) => {
     i18n.changeLanguage(value);
   };
+
+  const menu = [
+    {
+      title: t("menu.frist", { returnObjects: true })[0],
+      subtitle: t("menu.frist", { returnObjects: true })[1],
+    },
+    {
+      title: t("menu.second", { returnObjects: true })[0],
+      subtitle: t("menu.second", { returnObjects: true })[1],
+    },
+  ];
 
   return (
     <div className={isOpen ? "menu open" : "menu"}>
@@ -29,19 +32,33 @@ export const Menu = ({ isOpen, setMenuStatus }: any): JSX.Element => {
           setMenuStatus(false);
         }}
       >
-        close
+        {t("menu.close")}
       </p>
       <div className="menuContainer">
-        {menu.map((obj) => (
-          <div className="menuBox">
+        {menu.map((obj, index) => (
+          <div className="menuBox" key={index}>
             <p className="title">{obj.title}</p>
             <p className="subtitle">{obj.subtitle}</p>
           </div>
         ))}
       </div>
       <div className="languageBox">
-        <p className="auxiliar">PT</p>
-        <p className="principal">EN</p>
+        <p
+          className="secondary"
+          onClick={() => {
+            changeLanguage(languageSecondary);
+          }}
+        >
+          {languageSecondary === "br" ? "PT-BR" : "EN"}
+        </p>
+        <p
+          className="principal"
+          onClick={() => {
+            changeLanguage(language);
+          }}
+        >
+          {language === "br" ? "PT-BR" : "EN"}
+        </p>
       </div>
     </div>
   );
